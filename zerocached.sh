f06f5c94df33757edc6da1333cc15ac056955cfd
Joel Pinheiro <joel@opovodigital.com>
2018-04-16 19:00:51 -0300

    adjust comments

--- zerocached.sh
+++ zerocached.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# INFO:  SCRIPT QUE EFETUA A LIMPEZA DA MEMORIA CACHE, EVITANDO QUE A MESMA SEJA UTILIZADA E VA PARA A MEMORIA SWAP, CAUSANDO LENTIDAO NO SO
+# NOTA:  UTILIZA A LINGUAGEM DE CALCULADORA BC
+# AUTOR: JOEL PINHEIRO
+# EMAIL: JOEL@OPOVODIGITAL.COM
+# UPDATE 16/04/2018
 memlivre=$(cat /proc/meminfo | grep MemFree | tr -s ' ' | cut -d ' ' -f2) && memlivre=$(echo "$memlivre/1024.0" | bc)
 memcached=$(cat /proc/meminfo | grep "^Cached" | tr -s ' ' | cut -d ' ' -f2) && memcached=$(echo "$memcached/1024.0" | bc)
 
+# NA VARIAVEL ABAIXO ADICIONAR O LIMITE DE MEMORIA QUE O SERVER DEVE ALCANCAR PARA RODAR O COMANDO DENTRO DA CONDICAO A SEGUIR
 limite=`echo 1096`
 data=`date "+%d/%m/%Y, as %H:%M:%S"`
 
